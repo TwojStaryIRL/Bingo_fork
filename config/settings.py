4886e16eb5beaf1c5efc042090a6e8ac993a0e39
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-24 12:29:06 +0100

    requirements added game changed for top panel setting adjusted for deployed db

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wb6615*u^e2l-uepu!ecr=1knygpu1ndk94f*j6fib2w74jzlo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["*"]
 
 
 # import os
@@ -86,14 +86,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+# ++++++++++++++++++++++++++++++++++++++++++++++++
+# COMMENTED FOR NOW BELOW A KOYEB DB INSTANCE IS ADDED
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koyebdb',
+        'USER': 'koyeb-adm',
+        'PASSWORD': 'npg_17bgNThCLSKk',
+        'HOST': 'ep-crimson-mouse-agieznln.c-2.eu-central-1.pg.koyeb.app',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
