e85575d601e50dc0ba15336b8a6f9c8209278acc
Your Name <you@example.com>
2025-12-25 00:59:30 +0100

    i beg our lord and saviour Jesus Christ for making this work

--- config/urls.py
+++ config/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")), 
     path("game/save/", save_board, name="save_board"),
     path("raffle/", raffle, name="raffle"),
-    path("raffle/reroll/",raffle_reroll, name="raffle_reroll"),
+    path("raffle/reroll/", raffle_reroll, name="raffle_reroll"),
 ]
 
