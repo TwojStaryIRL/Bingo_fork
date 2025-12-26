2ef6e221d231e3eee763405538ca8361d2b432b9
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-26 14:50:56 +0100

    admin for new db model

--- bingo/admin.py
+++ bingo/admin.py
@@ -1,9 +1,14 @@
 # Register your models here.
 
 from django.contrib import admin
-from .models import BingoBoard
+from .models import BingoBoard, RaffleState
 
 @admin.register(BingoBoard)
 class BingoBoardAdmin(admin.ModelAdmin):
     list_display = ("user", "email", "updated_at")
     search_fields = ("user__username", "email")
+
+@admin.register(RaffleState)
+class RaffleStateAdmin(admin.ModelAdmin):
+    list_display = ("user", "rerolls_left", "shuffles_left", "updated_at")
+    search_fields = ("user__username",)
