d9333124b537c091e63595522a8edd9922ad40ac
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-28 14:30:48 +0100

    update settings

--- config/settings.py
+++ config/settings.py
@@ -10,14 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get('SECRET_KEY')#'django-insecure-wb6615*u^e2l-uepu!ecr=1knygpu1ndk94f*j6fib2w74jzlo'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY"
+)  #'django-insecure-wb6615*u^e2l-uepu!ecr=1knygpu1ndk94f*j6fib2w74jzlo'
 DEBUG = False
-ALLOWED_HOSTS = ['comprehensive-meris-bingoorg-fb3c57a1.koyeb.app']
+ALLOWED_HOSTS = ["comprehensive-meris-bingoorg-fb3c57a1.koyeb.app"]
 
 # DISABLE_COLLECTSTATIC = 1
 # import os
@@ -26,49 +29,49 @@ ALLOWED_HOSTS = ['comprehensive-meris-bingoorg-fb3c57a1.koyeb.app']
 # DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 
-
-
+# dummycomment
+#
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'bingo',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "bingo",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
@@ -82,13 +85,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #     }
 # }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST'),
-        'OPTIONS': {'sslmode': 'require'},
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("NAME"),
+        "USER": os.environ.get("USER"),
+        "PASSWORD": os.environ.get("PASSWORD"),
+        "HOST": os.environ.get("HOST"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
@@ -97,16 +100,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -114,9 +117,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -125,11 +128,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-#nasze pliki statyczne
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-#pliki statyczne już w deploymencie
-STATICROOT = os.path.join(BASE_DIR, 'staticfiles')
+# nasze pliki statyczne
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+# pliki statyczne już w deploymencie
+STATICROOT = os.path.join(BASE_DIR, "staticfiles")
 # file caching żeby przeglądarka się refreshowała i zbierała pliki typu cs js i obrazki przy każdym updacie
 STORAGES = {
     "staticfiles": {
@@ -137,7 +140,7 @@ STORAGES = {
     },
 }
 
-#redirecty po loginie logoucie itd.
+# redirecty po loginie logoucie itd.
 
 
 LOGIN_URL = "/"
@@ -145,4 +148,5 @@ LOGIN_REDIRECT_URL = "/game/"
 LOGOUT_REDIRECT_URL = "/"
 
 import django_heroku
+
 django_heroku.settings(locals())
