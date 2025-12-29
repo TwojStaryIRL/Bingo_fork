2e465aab16fa3c09030739b54739a2970cf89b0e
Wiktor Kowalski <wiktorkowalski70@gmail.com>
2025-12-29 14:40:33 +0100

    better rats

--- bingo/static/bingo/js/plugins/oniksu.js
+++ bingo/static/bingo/js/plugins/oniksu.js
@@ -29,7 +29,7 @@ window.BingoUserPlugin.init = function (api) {
     TILE_GAP: 14,
     SPEED_MIN: 36,
     SPEED_MAX: 72,
-    MARQUEE_OPACITY: 0.12,
+    MARQUEE_OPACITY: 0.22,
 
     AUDIO_VOL: 0.35,
   };
